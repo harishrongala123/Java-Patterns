619abb68ad6c17e6ed1bb6c1056dc728b5fa3b72
haris <harishrongala@gmail.com>
2023-09-09 16:11:27 +0530

    patterns

--- src/_87.java
+++ src/_87.java
@@ -1,11 +1,14 @@
-public class _86 {
+public class _87 {
     public static void main(String[] args) {
         for (int i = 70; i >= 65; i--) {
             for (int j = 65; j < i; j++) {
                 System.out.print("  ");
             }
-            for (int j = 65 +70 -i; j >= 65; j--) {
-                System.out.print((char) j + " ");
+            for (int j = 65+ 70-i; j >= 65; j--) {
+                if(j == 65+ 70-i || j == 65|| i == 65){
+                    System.out.print((char)j+ " ");
+                }else
+                    System.out.print(0+ " ");
             }
             System.out.println();
         }
