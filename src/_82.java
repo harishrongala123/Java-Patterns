ef98af883f7167c9d520a0d7db531b96f521c1c1
haris <harishrongala@gmail.com>
2023-09-09 14:54:07 +0530

    patterns

---
+++ src/_82.java
@@ -0,0 +1,13 @@
+public class _82 {
+    public static void main(String[] args) {
+        for (int i = 6; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i+1; j <= 7; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/_83.java
@@ -0,0 +1,5 @@
+public class _83 {
+    public static void main(String[] args) {
+
+    }
+}
