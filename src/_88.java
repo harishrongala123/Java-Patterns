ddb6dbf83bcb3930c1d5ac957769525ef205ba80
haris <harishrongala@gmail.com>
2023-09-25 17:23:07 +0530

    ]

--- src/_88.java
+++ src/_88.java
@@ -1,5 +1,19 @@
 public class _88 {
     public static void main(String[] args) {
+        for (int i = 70; i >= 65; i--) {
+            for (int j = 65; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 65+70-i; j >= 65; j--) {
+                int a = 0;
+                if(j ==65+70-i || j == 65 || i == 65) {
+                    System.out.print((char) j + " ");
+                }else {
+                    System.out.print(a++ +a + " ");
+                }
 
+            }
+            System.out.println();
+        }
     }
 }
